3eb027692ebd5f2975452da80b7e612d7d3a7f3e
jonishi <jonishi@iis.u-tokyo.ac.jp>
2013-12-05 23:39:37 +0900

    Added a statement.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char** argv) {
 	std::cout << "ok2" << std::endl;
 	std::cout << "ok3" << std::endl;
 	std::cout << "ok4" << std::endl;
+	std::cout << "ok5" << std::endl;
 	return 0;
 }
 
